b08b490f6ad5e165bc5d2ce9de1ef75eca76bdba
david-campos <david.campos.r96@gmail.com>
2020-05-12 00:02:48 +0200

    Trivia

--- commands/trivia.js
+++ commands/trivia.js
@@ -19,16 +19,17 @@ module.exports = {
         async execute(message, args, context) {
             try {
                 const response = await axios.get("https://opentdb.com/api.php?amount=1");
-                const answer = response.results[0];
+                const answer = response.data.results[0];
                 console.log(JSON.stringify(answer));
                 const answers = answer.incorrect_answers.slice(0);
                 answers.splice(Math.round(Math.random() * answers.length), 0, answer.correct_answer);
                 const embed = new MessageEmbed()
-                    .setTitle(answer.category)
+                    .setTitle(DIFFICULTY[answer.difficulty] + ' ' + answer.category)
                     .setColor(0xff7700)
-                    .setImage(answer.question + "\n" + answers.join("\n"));
+                    .setDescription(answer.question + "\n" + answers.join("\n"));
                 message.channel.send(embed).then();
             } catch (err) {
+                console.error(err);
                 message.reply('lo siento, no he podido obtener una pregunta.')
             }
         }
