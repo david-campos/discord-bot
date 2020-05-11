122983f273efd7b4af043c93bb45804baeab4cb8
david-campos <david.campos.r96@gmail.com>
2020-05-11 23:36:40 +0200

    Cambiado bote de jokes porque eran muy malas

--- commands/joke.js
+++ commands/joke.js
@@ -24,17 +24,14 @@ module.exports = {
          */
         async execute(message, args, context) {
             try {
-                const response = await axios.get("https://sv443.net/jokeapi/v2/joke/Any");
-                if (response.data.type === 'twopart') {
-                    context.lockMessageReception(message.channel, msg => {
-                        if (msg.author.bot) return;
-                        context.unlockMessageReception(message.channel);
-                        message.channel.send(response.data.delivery).then();
-                    });
-                    await message.channel.send(response.data.setup);
-                } else {
-                    await message.channel.send(response.data.joke);
-                }
+                const response = await axios.get("https://official-joke-api.appspot.com/jokes/random");
+                // if (response.data.type === 'twopart') {
+                await message.channel.send(response.data.setup);
+                setTimeout(() => message.channel.send(response.data.punchline).then(),
+                    2500);
+                // } else {
+                //     await message.channel.send(response.data.joke);
+                // }
             } catch (err) {
                 message.reply('lo siento, pero no hay chistes disponibles.')
             }
