8c50c453cef20734911b19f349c05c0d5e00ba28
david-campos <david.campos.r96@gmail.com>
2020-05-22 13:28:08 +0200

    Locales de eltiempo

--- commands/eltiempo.js
+++ commands/eltiempo.js
@@ -1,13 +1,19 @@
 const moment = require('moment');
 const {MessageEmbed} = require('discord.js');
 
+const LOCALES = ["af", "ar-dz", "ar-kw", "ar-ly", "ar-ma", "ar-sa", "ar-tn", "ar", "az", "be", "bg", "bm", "bn", "bo", "br", "bs", "ca", "cs", "cv", "cy", "da", "de-at", "de-ch", "de", "dv", "el", "en-au", "en-ca", "en-gb", "en-ie", "en-nz", "eo", "es-do", "es-us", "es", "et", "eu", "fa", "fi", "fo", "fr-ca", "fr-ch", "fr", "fy", "gd", "gl", "gom-latn", "gu", "he", "hi", "hr", "hu", "hy-am", "id", "is", "it", "ja", "jv", "ka", "kk", "km", "kn", "ko", "ky", "lb", "lo", "lt", "lv", "me", "mi", "mk", "ml", "mr", "ms-my", "ms", "mt", "my", "nb", "ne", "nl-be", "nl", "nn", "pa-in", "pl", "pt-br", "pt", "ro", "ru", "sd", "se", "si", "sk", "sl", "sq", "sr-cyrl", "sr", "ss", "sv", "sw", "ta", "te", "tet", "th", "tl-ph", "tlh", "tr", "tzl", "tzm-latn", "tzm", "uk", "ur", "uz-latn", "uz", "vi", "x-pseudo", "yo", "zh-cn", "zh-hk", "zh-tw"];
+
 module.exports = {
     commands: [{
         name: 'eltiempo',
         shortDescription: 'Hora actual',
-        usage: [{name: 'locale', optional: true,
+        description: 'Hora actual en ESPAÑA.',
+        usage: [{
+            name: 'locale', optional: true,
             description: 'código del locale a emplear para formatear la fecha y hora',
-        format: `uno de estos: ${moment.locales().join('|')}`}],
+            format: `uno de estos: ${LOCALES.join(', ')}`,
+            defaultValue: moment.locale
+        }],
         /**
          * @param {module:"discord.js".Message} message
          * @param {string[]} args
