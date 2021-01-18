c442d6391bbe3e7b7eaf4906c9cb384cfaf219fa
david-campos <david.campos.r96@gmail.com>
2021-01-18 20:30:53 +0100

    Converting capital into a single command

--- commands/capital.js
+++ commands/capital.js
@@ -1,10 +1,9 @@
-const countries = require('world-countries')
+const capital = require('world-countries')
 const {RIGHT, WRONG} = require("../guess_quizz/emojis");
 const {GuessingController} = require("../guess_quizz/guess_quizz");
 const {MessageEmbed} = require('discord.js');
 const config = require('../bot-config.json');
 const {DEFAULT_SPEEDRUN_LENGTH} = require("../guess_quizz/guess_quizz");
-
 
 /**
  * @extends GuessingController<Country>
@@ -85,10 +84,10 @@ class CapitalController extends GuessingController {
     }
 }
 
-const capitalController = new CapitalController(countries);
+const capitalController = new CapitalController(capital);
 const tempRegions = new Set();
 const subregions = new Map();
-countries.forEach(ctr => {
+capital.forEach(ctr => {
     tempRegions.add(ctr.region);
     if (ctr.subregion.length > 0) {
         if (subregions.has(ctr.region)) {
@@ -100,6 +99,40 @@ countries.forEach(ctr => {
 });
 const regions = new Array(...tempRegions);
 
+const SUBCOMMANDS = {
+    'speed': async (msg, args, bot) => {
+        const independent = args.length > 0 && args[0] === "independent";
+        if (independent) {
+            args = args.slice(1);
+        }
+        const hasRegion = args.length > 0 && isNaN(parseInt(args[0], 10));
+        if (!independent && !hasRegion) {
+            await capitalController.cmdSpeedRunStart(msg, args, bot);
+        } else {
+            const lastIsNumber = args.length > 0 && !isNaN(parseInt(args[args.length - 1], 10));
+            const newArgs = lastIsNumber ? args.slice(args.length - 1) : [];
+            const filtroIdp = independent ? (ctr => ctr.independent) : (() => true);
+            if (hasRegion) {
+                const region = args.slice(0, lastIsNumber ? args.length - 1 : args.length).join(' ');
+                const filtroRegion = ctr => ctr.region === region || ctr.subregion === region;
+                await capitalController.cmdSpeedRunStart(msg, newArgs, bot,
+                    ctr => filtroRegion(ctr) && filtroIdp(ctr));
+            } else {
+                await capitalController.cmdSpeedRunStart(msg, newArgs, bot, ctr => ctr.independent);
+            }
+        }
+    },
+    'regions': async (msg, args, bot) => {
+        msg.reply(
+            'Regions (with subregions between parenthesis):\n'
+            + regions.map(rg => subregions.has(rg) ?
+            `${rg}: ${[...subregions.get(rg)].join(', ')}` : rg).join('\n')
+        );
+    },
+    'hint': capitalController.cmdHint.bind(capitalController),
+    'expert': capitalController.cmdExpertRunStart.bind(capitalController)
+};
+
 /**
  * @type {CommandExports}
  */
@@ -110,85 +143,63 @@ module.exports = {
             shortDescription: 'Acierta la capital (en inglés)',
             description: 'Gives a random country to guess the capital or guesses the capital for the current country obtained this way',
             usage: [
-                {name: 'guess', description: 'capital you guess for the given country', optional: true}
-            ],
-            execute: capitalController.cmdBasic.bind(capitalController)
-        },
-        {
-            name: 'capital-speed',
-            shortDescription: 'Speed-run de capitales (en inglés)',
-            description: 'Initiates a capital speedrun. During the speedrun anyone can answer, countries will come one after the other, the first person to answer the correct capital get the point.',
-            usage: [
                 {
-                    name: 'independent',
-                    description: 'only independent countries capitals',
-                    optional: true,
-                    isLiteral: true
+                    subcommand: 'Basic', description: 'Gives a capital to guess and allows to guess it',
+                    args: [{name: 'guess', description: 'capital you guess for the given country', optional: true}]
                 },
                 {
-                    name: 'region',
-                    description: 'region or sub-region, check regions with -capital-regions',
-                    optional: true,
-                    format: 'check -capital-regions for valid values',
-                    defaultValue: 'null'
+                    subcommand: 'Hint',
+                    description: 'Gives a hint for the current capital guess',
+                    args: [{name: 'hint', isLiteral: true}]
                 },
                 {
-                    name: 'N',
-                    description: 'number of countries in the speedrun',
-                    optional: true,
-                    format: 'positive integer',
-                    defaultValue: DEFAULT_SPEEDRUN_LENGTH
+                    subcommand: 'Capital speed',
+                    description: 'Initiates a capital speedrun. During the speedrun anyone can answer, countries will come one after the other, the first person to answer the correct capital get the point.',
+                    args: [
+                        {
+                            name: 'speed',
+                            isLiteral: true
+                        },
+                        {
+                            name: 'independent',
+                            description: 'only independent countries capitals',
+                            optional: true,
+                            isLiteral: true
+                        },
+                        {
+                            name: 'region',
+                            description: 'region or sub-region, check regions with -capital-regions',
+                            optional: true,
+                            format: 'check -capital-regions for valid values',
+                            defaultValue: 'null'
+                        },
+                        {
+                            name: 'N',
+                            description: 'number of countries in the speedrun',
+                            optional: true,
+                            format: 'positive integer',
+                            defaultValue: DEFAULT_SPEEDRUN_LENGTH
+                        }
+                    ]
+                },
+                {
+                    subcommand: 'Regions',
+                    description: 'Lists the possible regions and subregions to filter the capitals when starting a speedrun.',
+                    args: [{name: 'regions', isLiteral: true}]
+                },
+                {
+                    subcommand: 'Expert',
+                    description: 'Start an expert run, a challenge not made for the faint of heart!',
+                    args: [{name: 'expert', isLiteral: true}]
                 }
             ],
             execute: async (msg, args, bot) => {
-                const independent = args.length > 0 && args[0] === "independent";
-                if (independent) {
-                    args = args.slice(1);
-                }
-                const hasRegion = args.length > 0 && isNaN(parseInt(args[0], 10));
-                if (!independent && !hasRegion) {
-                    await capitalController.cmdSpeedRunStart(msg, args, bot);
+                if (args.length > 0 && (args[0] in SUBCOMMANDS)) {
+                    SUBCOMMANDS[args[0]](msg, args.slice(1), bot);
                 } else {
-                    const lastIsNumber = args.length > 0 && !isNaN(parseInt(args[args.length - 1], 10));
-                    const newArgs = lastIsNumber ? args.slice(args.length - 1) : [];
-                    const filtroIdp = independent ? (ctr => ctr.independent) : (() => true);
-                    if (hasRegion) {
-                        const region = args.slice(0, lastIsNumber ? args.length - 1 : args.length).join(' ');
-                        const filtroRegion = ctr => ctr.region === region || ctr.subregion === region;
-                        await capitalController.cmdSpeedRunStart(msg, newArgs, bot,
-                            ctr => filtroRegion(ctr) && filtroIdp(ctr));
-                    } else {
-                        await capitalController.cmdSpeedRunStart(msg, newArgs, bot, ctr => ctr.independent);
-                    }
+                    capitalController.cmdBasic(msg, args, bot);
                 }
             }
-        },
-        {
-            name: 'capital-regions',
-            shortDescription: 'Regiones para capitales (en inglés)',
-            description: 'Lists the possible regions and subregions to filter the capitals when starting a speedrun.',
-            usage: [],
-            execute: async (msg, args, bot) => {
-                msg.reply(
-                    'Regions (with subregions between parenthesis):\n'
-                    + regions.map(rg => subregions.has(rg) ?
-                    `${rg}: ${[...subregions.get(rg)].join(', ')}` : rg).join('\n')
-                );
-            }
-        },
-        {
-            name: 'capital-hint',
-            shortDescription: 'Pista de capitales (en inglés)',
-            description: 'Gives a hint for the current capital guess',
-            usage: [],
-            execute: capitalController.cmdHint.bind(capitalController)
-        },
-        {
-            name: 'capital-expert',
-            shortDescription: 'Expert-run de capitales (en inglés)',
-            description: 'Start an expert run, a challenge not made for the faint of heart!',
-            usage: [],
-            execute: capitalController.cmdExpertRunStart.bind(capitalController)
         },
         {
             name: 'ctr-info',
@@ -210,22 +221,22 @@ module.exports = {
                 let country = null;
                 if (args[0].length === 2) {
                     const code = args[0].toUpperCase();
-                    country = countries.find(c => c.cca2 === code);
+                    country = capital.find(c => c.cca2 === code);
                 }
                 if (!country && args[0].length === 3) {
                     const code = args[0].toUpperCase();
                     if (/^[0-9]+$/.test(code)) {
-                        country = countries.find(c => c.ccn3 === code);
+                        country = capital.find(c => c.ccn3 === code);
                     } else {
-                        country = countries.find(c => c.cca3 === code);
+                        country = capital.find(c => c.cca3 === code);
                     }
                 }
                 if (!country) {
                     const f = x => x.toLowerCase().normalize("NFD").replace(/[^A-Za-z]+/g, '');
                     const normalized = f(args[0]);
-                    country = countries.find(c => f(c.name.common) === normalized || f(c.name.official) === normalized)
-                        || countries.find(c => c.altSpellings.find(s => f(s) === normalized) !== undefined)
-                        || countries.find(c =>
+                    country = capital.find(c => f(c.name.common) === normalized || f(c.name.official) === normalized)
+                        || capital.find(c => c.altSpellings.find(s => f(s) === normalized) !== undefined)
+                        || capital.find(c =>
                             Object.values(c.translations).find(
                                 t => f(t.official) === normalized || f(t.common) === normalized
                             ) !== undefined
