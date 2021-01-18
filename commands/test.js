c7936a136b1470f37e6f8588ce09297196eaebcc
david-campos <david.campos.r96@gmail.com>
2021-01-18 17:28:02 +0100

    A test command

---
+++ commands/test.js
@@ -0,0 +1,22 @@
+const {MessageEmbed} = require('discord.js');
+const emoji = require('../emojis2');
+
+module.exports = {
+    commands: [{
+        name: 'test',
+        hidden: true,
+        shortDescription: 'Comando secreto de testear cosas.',
+        description: `No deberías conocer esto, LARGO! ${emoji.ANGRY_FACE}`,
+        /**
+         * @param {module:"discord.js".Message} message
+         * @param {string[]} args
+         * @param {Bot} context
+         */
+        async execute(message, args, context) {
+            args.forEach(arg => console.log(
+                /\p{Emoji}/u.test(arg),
+                arg.charCodeAt(0),
+                Object.entries(emoji).find(([k, v]) => arg === v)));
+        }
+    }]
+}
